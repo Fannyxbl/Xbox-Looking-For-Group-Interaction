fd785745e65466ff57164397ddfb4fae8ef12c81
Emily <90411859+adorably@users.noreply.github.com>
2022-06-14 17:57:25 -0400

    Update user_info.py
    added users' gamertag setting

--- source/user_info.py
+++ source/user_info.py
@@ -12,11 +12,12 @@ class User_Info:
                 'x-xbl-contract-version': '2'
             }
 
-            async with session.get('https://profile.xboxlive.com/users/me/profile/settings', headers=headers) as user_info_request:
+            async with session.get('https://profile.xboxlive.com/users/me/profile/settings?settings=Gamertag', headers=headers) as user_info_request:
                 if user_info_request.status == 200:
                     response_data = await user_info_request.json()
                     xuid = response_data['profileUsers'][0]['id']
-                    print(f'\n \x1b[1;39m[\x1b[1;36m*\x1b[1;39m] Retrieved Profile Information! \n [\x1b[1;36m*\x1b[39m] My XUID: \x1b[1;36m{xuid}\n')
+                    gamertag = response_data['profileUsers'][0]['settings'][0]['value']
+                    print(f'\n \x1b[1;39m[\x1b[1;36m*\x1b[1;39m] Retrieved Profile Information! \n [\x1b[1;36m*\x1b[39m] My Gamertag: \x1b[1;36m{gamertag}\n [\x1b[1;36m*\x1b[39m] My XUID: \x1b[1;36m{xuid}\n')
                     return xuid
                 else:
                     print(' \x1b[1;39m[\x1b[1;31m!\x1b[1;39m] Failed to retrieve user info!')
